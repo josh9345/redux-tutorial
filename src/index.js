5aed5af6310b81ea9bb686970f2e7108636f8527
Joshua Reed <joshualreed93@gmail.com>
2020-06-14 12:42:39 -0400

    reducer complete

--- src/index.js
+++ src/index.js
@@ -4,37 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
+import rootReducer from "./reducers";
 
-// Store
-
-// Action
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-//Reducer
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-let store = createStore(counter);
-
-//Display
-store.subscribe(() => console.log(store.getState()));
-
-// Dispatch
-store.dispatch(increment());
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import counterReducer from "./counter";
+import loggedReducer from "./islogged";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  isLogged: loggedReducer,
+});
+export default rootReducer;
